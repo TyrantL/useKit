739d8f7a9b3131db6d3b888e55cc93a3861b16d6
lzez <lzez@servyou.com.cn>
2025-03-04 11:17:24 +0800

    feat(hooks): 新增 useModal 钩子
    - 添加 useCommonRef 函数，用于创建和管理模态框
    - 定义 ModalType、ModalAction、ModalRefOption 等类型
    - 实现模态框的弹出、关闭、数据传递等功能
    - 优化 biome 配置，增加对 noExplicitAny 的警告

---
+++ src/hooks/useModal/common.ts
@@ -0,0 +1,246 @@
+import {
+	useCallback,
+	useImperativeHandle,
+	useMemo,
+	useRef,
+	useState,
+} from 'react';
+import { isFunction } from '../utils';
+import type {
+	ModalAction,
+	ModalPropsTypeMap,
+	ModalRef,
+	ModalRefOption,
+	ModalType,
+} from './types';
+
+const modalTypeMap: Record<
+	ModalType,
+	{
+		visible: string;
+		onClose: string;
+	}
+> = {
+	modal: {
+		visible: 'open',
+		onClose: 'onCancel',
+	},
+	drawer: {
+		visible: 'open',
+		onClose: 'onClose',
+	},
+};
+
+function resolveDefaultData<T>(data: T | (() => T)) {
+	if (!data) return data;
+	if (isFunction(data)) return data();
+	return data;
+}
+
+function useCommonRef<P extends ModalType, T, U>(
+	modalType: P,
+	ref: React.Ref<any>,
+	options: ModalRefOption<P, T, U>,
+	defaultData: Partial<T> | (() => Partial<T>) = {},
+) {
+	const [props, setProps] = useState<{
+		visible:
+			| false
+			| { resolve: (value: U) => void; reject: (reason: any) => void };
+		data: Partial<T>;
+		options: ModalRefOption<P, T, U>;
+		promise: null | Promise<U> | PromiseLike<U>;
+	}>(() => ({
+		visible: false,
+		data: resolveDefaultData(defaultData),
+		options,
+		promise: null,
+	}));
+
+	const $refs = useRef(
+		{} as { props: typeof props; defaultData: typeof defaultData },
+	);
+
+	$refs.current.props = props;
+	$refs.current.defaultData = defaultData;
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	const modal = useMemo<ModalRef<P, T, U>>(() => {
+		const ret: ModalRef<P, T, U> = {
+			get visible() {
+				return Boolean($refs.current.props.visible);
+			},
+			get data(): Partial<T> {
+				return $refs.current.props.data;
+			},
+			get props() {
+				const map = modalTypeMap[modalType];
+				return {
+					[map.visible]: Boolean($refs.current.props.visible),
+					[map.onClose]: this.cancelModal,
+				} as ModalPropsTypeMap[P];
+			},
+			get modalPromise() {
+				return $refs.current.props.promise;
+			},
+			get options() {
+				return $refs.current.props.options;
+			},
+
+			modal(newData: Partial<T> = {}): Promise<U> {
+				// biome-ignore lint/suspicious/noAsyncPromiseExecutor: <explanation>
+				const promise = new Promise<U>(async (resolve, reject) => {
+					if ($refs.current.props.visible) {
+						return;
+					}
+
+					// 将传入的数据和defaultData合并赋值给newModalData
+					const defaultData = resolveDefaultData(
+						$refs.current.defaultData || {},
+					);
+					let newModalData: Partial<T> = {
+						...defaultData,
+						...newData,
+					};
+
+					const { beforeModal, afterModal, beforeCloseModal, afterCloseModal } =
+						this.options;
+
+					if (beforeModal) {
+						let pauseResult: any;
+						let pause = false;
+
+						const pauseFn = (result: any, _isError?: boolean) => {
+							pause = true;
+							pauseResult = result;
+						};
+
+						// 执行beforeModal
+						const result =
+							(await beforeModal(newModalData, pauseFn)) || newModalData;
+						// 如果有result,将newModalData和result合并，防止传入的newData丢失
+						if (result && result !== newModalData) {
+							newModalData = {
+								...defaultData,
+								...result,
+							};
+						}
+
+						// 如果beforeModal中调用了第二个参数，则视为希望暂停弹窗弹出
+						if (pause) {
+							reject(pauseResult);
+						}
+
+						const closeFn = (
+							before: (next: () => void) => void,
+							action: ModalAction,
+						) => {
+							const close = async () => {
+								const _close = () => {
+									Object.assign($refs.current.props, {
+										visible: false,
+										promise: null,
+									});
+
+									setProps($refs.current.props);
+
+									setTimeout(() => {
+										afterCloseModal?.(this.data, action, this);
+									});
+								};
+
+								if (before) {
+									before(_close);
+								} else {
+									_close();
+								}
+							};
+
+							const modalClose = () => {
+								if (beforeCloseModal) {
+									beforeCloseModal(close, action, this);
+								} else {
+									close();
+								}
+							};
+
+							modalClose();
+						};
+
+						Object.assign($refs.current.props, {
+							data: newModalData,
+							visible: {
+								resolve: (value: U) => {
+									return closeFn((next) => {
+										resolve(value);
+										next();
+									}, 'end');
+								},
+								reject: (reason: any) => {
+									closeFn((next) => {
+										reject(reason);
+										next();
+									}, 'cancel');
+								},
+							},
+						});
+
+						setProps({ ...$refs.current.props });
+
+						setTimeout(() => {
+							afterModal?.(newData);
+						});
+					}
+				});
+
+				$refs.current.props.promise = promise;
+				return promise;
+			},
+
+			endModal: (result: U) => {
+				if ($refs.current.props.visible) {
+					$refs.current.props.visible.resolve(result);
+				}
+			},
+
+			cancelModal: (reason) => {
+				if ($refs.current.props.visible) {
+					$refs.current.props.visible.reject(reason);
+				}
+			},
+		};
+		return ret;
+	}, []);
+
+	const setData = useCallback(
+		(newData: Partial<T> | ((data: Partial<T>) => Partial<T>)) => {
+			if (isFunction(newData)) {
+				return setProps((p) => ({
+					...p,
+					data: {
+						...p.data,
+						...newData(p.data),
+					},
+				}));
+			}
+			return setProps({
+				...$refs.current.props,
+				data: {
+					...$refs.current.props.data,
+					...newData,
+				},
+			});
+		},
+		[],
+	);
+
+	useImperativeHandle(ref, () => modal, [modal]);
+
+	return {
+		modal,
+		data: modal.data,
+		setData,
+	};
+}
+
+export default useCommonRef;
---
+++ src/hooks/useModal/types.ts
@@ -0,0 +1,45 @@
+export type ModalType = 'modal' | 'drawer';
+
+export type ModalAction = 'end' | 'cancel';
+
+export type ModalRefOption<P extends ModalType, T, U> = {
+	beforeModal?: (
+		data: Partial<T>,
+		pause: (result: any, isError?: boolean) => void,
+	) => undefined | T | Promise<undefined | T>;
+	afterModal?: (data: Partial<T>) => void;
+	beforeCloseModal?: (
+		next: () => void,
+		action: ModalAction,
+		modal: ModalRef<P, T, U>,
+	) => void | Promise<void>;
+	afterCloseModal?: (
+		data: Partial<T>,
+		action: ModalAction,
+		modal: ModalRef<P, T, U>,
+	) => void | Promise<void>;
+};
+
+export type ModalPropsTypeMap = {
+	modal: {
+		visible: boolean;
+		onClose: () => void;
+	};
+	drawer: {
+		visible?: boolean;
+		onClose?: () => void;
+	};
+};
+
+export interface ModalRef<P extends ModalType, T, U> {
+	readonly visible: boolean;
+	readonly data: Partial<T>;
+	readonly props: ModalPropsTypeMap[P];
+	readonly options: ModalRefOption<P, T, U>;
+	readonly modalPromise: null | Promise<U> | PromiseLike<U>;
+
+	modal(newData: T): Promise<U>;
+
+	endModal: (result: U) => void;
+	cancelModal: (reason: any) => void;
+}
