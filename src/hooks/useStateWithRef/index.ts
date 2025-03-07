dd8af0ab7cdd771044c9ea356fbab3d9b1f59cad
lzez <lzez@servyou.com.cn>
2025-03-07 09:40:43 +0800

    refactor(hooks): 优化 useStateWithRef 的代码结构和类型定义

--- src/hooks/useStateWithRef/index.ts
+++ src/hooks/useStateWithRef/index.ts
@@ -1,29 +1,34 @@
-import React, { Dispatch, SetStateAction, useCallback, useRef } from 'react';
+import React, {
+	type Dispatch,
+	type SetStateAction,
+	useCallback,
+	useRef,
+} from 'react';
 
-type GetStateAction<S> = () => S
+type GetStateAction<S> = () => S;
 
 function useStateWithRef<S>(
-  initialState: S | (() => S)
-): [S, Dispatch<SetStateAction<S>>, GetStateAction<S>]
+	initialState: S | (() => S),
+): [S, Dispatch<SetStateAction<S>>, GetStateAction<S>];
 
 function useStateWithRef<S = undefined>(): [
-    S | undefined,
-  Dispatch<SetStateAction<S | undefined>>,
-  GetStateAction<S | undefined>
-]
+	S | undefined,
+	Dispatch<SetStateAction<S | undefined>>,
+	GetStateAction<S | undefined>,
+];
 
 function useStateWithRef<S>(initialState?: S) {
-  const [state, setState] = React.useState(initialState)
-  const stateRef = useRef(initialState)
+	const [state, setState] = React.useState(initialState);
+	const stateRef = useRef(initialState);
 
-  const getState = useCallback(() => stateRef.current, [])
+	const getState = useCallback(() => stateRef.current, []);
 
-  const setStateWithRef = useCallback((value: S) => {
-    setState(value)
-    stateRef.current = value
-  }, [])
+	const setStateWithRef = useCallback((value: S) => {
+		setState(value);
+		stateRef.current = value;
+	}, []);
 
-  return [state, setStateWithRef, getState]
+	return [state, setStateWithRef, getState];
 }
 
-export default useStateWithRef
+export default useStateWithRef;
